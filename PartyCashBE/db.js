fefa6314fa87c896efabb6792997da9cfdab5c71
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-09 14:10:58 +0200

    fixed db hostname  into partybudget_db

--- PartyCashBE/db.js
+++ PartyCashBE/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'admin',
-    host: 'localhost',
+    host: 'partybudget_db',
     database: 'partybudget',
     password: 'admin',
     port: 5432,
