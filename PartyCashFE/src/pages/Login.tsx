a7bbb65474484e152bb354a211d60c27a652f19a
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-11 22:46:32 +0200

    new login page

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -6,11 +6,11 @@ export default function Login() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
-      <div className="max-w-md w-full p-8 bg-white shadow-xl rounded-lg">
+      <div className="max-w-md w-full p-8 bg-white shadow-xl rounded-lg flex flex-col items-center">
         {/* Header con logo e titolo PartyCash */}
-        <div className="flex items-center justify-center mb-6">
-          <div className="w-12 h-12 mr-3">
-            <img src={logo} alt="PartyCash Logo" className="w-full h-full" />
+        <div className="flex flex-col items-center mb-6">
+          <div className="w-24 h-24 mb-4">
+            <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
           </div>
           <h1 className="text-3xl font-bold text-cyan-900">PartyCash</h1>
         </div>
