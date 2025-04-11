bd4a95d0086ca939c5056db2e74e0261bca77db6
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-12 01:37:19 +0200

    new login page update5

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -6,7 +6,7 @@ export default function Login() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
-      <div className="w-1/2 max-w-7xl p-8 flex flex-col lg:flex-row items-center justify-center space-x-4 bg-white shadow-xl rounded-lg">
+      <div className="w-max max-w-7xl p-8 flex flex-col lg:flex-row  justify-center space-x-4 bg-white shadow-xl rounded-lg">
         {/* Left side: Logo */}
         <div className="flex justify-center lg:justify-center lg:w-1/2 w-full mb-4 lg:mb-0">
           <div className="w-60 h-60">
@@ -20,7 +20,7 @@ export default function Login() {
       </div>
 
       {/* Footer with copyright positioned on the bottom-right */}
-      <footer className="absolute bottom-4 right-4 text-white text-sm">
+      <footer className="flex bottom-4 right-4 text-cyan-900 text-sm">
         &copy; {currentYear} B.Torregrossa
       </footer>
     </div>
