cb9e52a5d3c3131ed76703a5f18c222984875f61
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-11 23:19:50 +0200

    new login page logo bigger no title indexhtml updated

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import LoginForm from '@/features/auth/LoginForm'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 
 export default function Login() {
   const currentYear = new Date().getFullYear()
@@ -8,8 +8,8 @@ export default function Login() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
       <div className="max-w-md w-full p-8 bg-white shadow-xl rounded-lg flex flex-col items-center">
         {/* Header con logo e titolo PartyCash */}
-        <div className="flex flex-col items-center mb-2"> {/* Reduced margin-bottom */}
-          <div className="w-80 h-80">
+        <div className="flex flex-col items-center"> {/* Reduced margin-bottom */}
+          <div className="w-60 h-60">
             <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
           </div>
         </div>
