988d6be8a490d29b1093e36c84b096843073ed12
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-12 01:19:34 +0200

    new login page update4

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -6,15 +6,15 @@ export default function Login() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
-      <div className="w-full max-w-7xl p-8 flex flex-col lg:flex-row items-center justify-center space-x-4 bg-white shadow-xl rounded-lg">
+      <div className="w-1/2 max-w-7xl p-8 flex flex-col lg:flex-row items-center justify-center space-x-4 bg-white shadow-xl rounded-lg">
         {/* Left side: Logo */}
-        <div className="flex justify-center lg:justify-start lg:w-1/3 w-full mb-4 lg:mb-0">
+        <div className="flex justify-center lg:justify-center lg:w-1/2 w-full mb-4 lg:mb-0">
           <div className="w-60 h-60">
             <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
           </div>
         </div>
         {/* Right side: Login Form */}
-        <div className="w-full lg:w-2/3">
+        <div className="w-full lg:w-2/2">
           <LoginForm />
         </div>
       </div>
