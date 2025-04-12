c8b21c718ec7f90de37143a3a8c6ca675f1e4aab
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-12 02:17:23 +0200

    new login page update8

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -6,25 +6,21 @@ export default function Login() {
 
   return (
     <div className="min-h-screen flex justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
-      <div className="w-max max-w-7xl p-4 flex flex-col lg:flex-row space-x-4 bg-white shadow-xl rounded-lg">
+      <div className="w-full max-w-7xl p-4 flex flex-col lg:flex-row items-center justify-between space-x-0 lg:space-x-4 bg-white shadow-xl rounded-lg">
         {/* Left side: Logo */}
-        <div className="flex justify-center lg:justify-center lg:w-1/2 w-full mb-4 lg:mb-0">
+        <div className="flex justify-center w-full lg:w-1/2 mb-4 lg:mb-0">
           <div className="w-60 h-60">
             <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
           </div>
         </div>
         {/* Right side: Login Form */}
-        <div className="w-full lg:w-2/2">
+        <div className="w-full lg:w-1/2 flex justify-center">
           <LoginForm />
         </div>
-        <div className="flex justify-center p-8 text-cyan-900 text-sm">
+      </div>
+      <div className="flex justify-center p-8 text-cyan-900 text-sm">
         &copy; {currentYear} B.Torregrossa
       </div>
-
-      </div>
-
-      {/* Footer with copyright positioned on the bottom-right */}
-      
     </div>
   )
 }
