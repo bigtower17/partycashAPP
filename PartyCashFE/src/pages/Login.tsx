70f56927d4003e83a779ea55ec5f09ec0fa49c26
Beniamino Torregrossa <beniamino.torregrossa@visicomweb.it>
2025-04-12 02:24:56 +0200

    new login page update9

--- PartyCashFE/src/pages/Login.tsx
+++ PartyCashFE/src/pages/Login.tsx
@@ -5,22 +5,21 @@ export default function Login() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <div className="min-h-screen flex justify-center bg-gradient-to-br from-cyan-500 to-cyan-900">
-      <div className="w-full max-w-7xl p-4 flex flex-col lg:flex-row items-center justify-between space-x-0 lg:space-x-4 bg-white shadow-xl rounded-lg">
-        {/* Left side: Logo */}
-        <div className="flex justify-center w-full lg:w-1/2 mb-4 lg:mb-0">
-          <div className="w-60 h-60">
-            <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
-          </div>
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-cyan-500 to-cyan-900">
+      <div className="w-full max-w-7xl p-4 flex flex-col items-center bg-white shadow-xl rounded-lg">
+        {/* Logo */}
+        <div className="w-60 h-60 mb-6">
+          <img src={logo} alt="PartyCash Logo" className="w-full h-full object-contain" />
         </div>
-        {/* Right side: Login Form */}
-        <div className="w-full lg:w-1/2 flex justify-center">
+        {/* Login Form */}
+        <div className="w-full lg:w-2/3">
           <LoginForm />
         </div>
-      </div>
-      <div className="flex justify-center p-8 text-cyan-900 text-sm">
-        &copy; {currentYear} B.Torregrossa
-      </div>
+        {/* Copyright */}
+        <div className="flex justify-center p-8 text-cyan-900 text-sm mt-4">
+          &copy; {currentYear} B.Torregrossa
+        </div>
+      </div>   
     </div>
   )
 }
